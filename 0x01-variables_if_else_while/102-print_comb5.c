0840f1ebbbf21556eb4e1f085c05f01c032b528a
Mba Olumba <mbakanu90@gmail.com>
2022-12-10 03:03:48 -0800

    C

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,35 +2,34 @@
 #include <stdlib.h>
 
 /**
- *  * main - returns a combination of number
- *   * Return: return 0 if successful
- *    */
+ * main - returns a combination of number
+ * Return: return 0 if successful
+ */
 
 int main(void)
 {
-		int i, j;
-			
-			for (i = 0; i < 100; i++)
-					{
-								for (j = 0; j < 100; j++)
-											{
-															if (i < j)
-																			{
-																								putchar((i / 10) + 48);
-																												putchar((i % 10) + 48);
-																																putchar(' ');
-																																				putchar((j / 10) + 48);
-																																								putchar((j % 10) + 48);
-																																												if (i != 98 || j != 99)
-																																																	{
-																																																							putchar(',');
-																																																												putchar(' ');
-																																																																}
-																																															}
-																	}
-									}
-				
-				putchar('\n');
+	int i, j;
 
-					return (0);
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
 }
