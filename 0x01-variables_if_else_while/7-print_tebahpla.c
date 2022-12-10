7328e20a16d9a51073e5a9ca0eb94d597da22f47
Mba Olumba <mbakanu90@gmail.com>
2022-12-10 02:11:09 -0800

    C

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 
 /**
- *  * main - returns an alphabet in reverse order
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - returns an alphabet in reverse order
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-		int ch = 'z';
+	int ch = 'z';
 
-			while (ch >= 'a')
-					{
-								putchar(ch);
-										ch--;
-											}
-				putchar('\n');
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
 
-					return (0);
+	return (0);
 }
